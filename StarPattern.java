daf51157cc830dbfeae454bc0cdaddd313322322
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-15 02:38:28 +0530

    Update StarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -1,30 +1,14 @@
 public class StarPattern {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            if (i > 1) {
-                for (int j = 1; j <= (2 * i - 3); j++) {
+        int n = 4; // Distance from center to vertex
+        for (int i = -n; i <= n; i++) {
+            for (int j = -n; j <= n; j++) {
+                // The absolute sum defines the diamond boundary
+                if (Math.abs(i) + Math.abs(j) == n) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        for (int i = n - 1; i >= 1; i--) {
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            if (i > 1) {
-                for (int j = 1; j <= (2 * i - 3); j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
             }
             System.out.println();
         }
